320ba42c5b25224ce0f3f4d09e315af6bf772707
ojw92 <joh15@illinois.edu>
2025-10-04 22:09:14 -0400

    Updated color sampling clamp & Notes

--- RayTracing/interval.h
+++ RayTracing/interval.h
@@ -22,6 +22,13 @@ public:
         return min < x && x < max;
     }
 
+    // Ensure the color components of the averaged color remains in [0,1] bounds
+    double clamp(double x) const {
+        if (x < min) return min;
+        if (x > max) return max;
+        return x
+    }
+
     static const interval empty, universe;  // class-level constants shared by all instances of 'interval' class object; 'global constant'
         // much like class attribute some_class.x variable in a Python class object
         // not like self.x because that's for per-object members
