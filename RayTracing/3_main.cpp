53d1da2c70419ebc918dcdd7933acda7ce0414ad
ojw92 <joh15@illinois.edu>
2025-10-27 21:45:17 -0700

    Implemented metal's ray scatter on 3_main.cpp

--- RayTracing/3_main.cpp
+++ RayTracing/3_main.cpp
@@ -3,6 +3,7 @@
 #include "camera.h"
 #include "hittable.h"
 #include "hittable_list.h"
+#include "material.h"
 #include "sphere.h"
 
 // #include <iostream>
@@ -65,8 +66,15 @@ int main() {
     // World
     hittable_list world;    // collection of 'hittable' objects; its 'hit' loops over all objects, keeping the closest valid hit
 
-    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));      // small sphere visible at z=-1
-    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100)); // big sphere used as ground plane (radius=100, top is near y=0)
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
+
+    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));  // big sphere used as ground plane (radius=100, top is near y=0)
+    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));       // small sphere visible at z = -1.2
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     // Camera
     camera cam;
